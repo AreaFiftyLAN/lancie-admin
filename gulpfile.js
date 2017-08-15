6c6b0dc845060d89453f34063036e7a7968e785f
Sille Kamoen <sille.kamoen@gmail.com>
2017-08-15 15:35:11 +0200

    Polymer 2.0 Upgrade (#35)
    * Initial upgrade attempt
    
    * Remove old dependencies
    
    * Fix a bunch of linter errors and imports
    
    * Dynamic column creation for vaadin grid
    
    * Partially fix dynamic column creation
    
    * Fix columnrenderer, apply material design and add sorting
    
    * Add sorting icon and selection column
    
    * Fix webcomponents javascript and consumption dropdown
    
    * Fix build
    
    * Update build.sh for new build process
    
    * Clean dependencies and structured root directory
    
    * Add last style fixes and hide empty table

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,6 @@ http://polymer.github.io/PATENTS.txt
 
 const path = require('path');
 const gulp = require('gulp');
-const gulpif = require('gulp-if');
-const uglify = require('gulp-uglify');
-const cssSlam = require('css-slam').gulp;
-const htmlMinifier = require('gulp-html-minifier');
 const jshint = require('gulp-jshint');
 const superagent = require('superagent');
 const fs = require('fs-extra');
@@ -26,82 +22,13 @@ const glob = require('glob');
 // const logging = require('plylog');
 // logging.setVerbose();
 
-// !!! IMPORTANT !!! //
-// Keep the global.config above any of the gulp-tasks that depend on it
-global.config = {
-  polymerJsonPath : path.join(process.cwd(), 'polymer.json'),
-  build : {
-    rootDirectory : 'build',
-    bundledDirectory : 'bundled',
-    unbundledDirectory : 'unbundled',
-    // Accepts either 'bundled', 'unbundled', or 'both'
-    // A bundled version will be vulcanized and sharded. An unbundled version
-    // will not have its files combined (this is for projects using HTTP/2
-    // server push). Using the 'both' option will create two output projects,
-    // one for bundled and one for unbundled
-    bundleType : 'both'
-  },
-  // Path to your service worker, relative to the build root directory
-  serviceWorkerPath : 'service-worker.js',
-  // Service Worker precache options based on
-  // https://github.com/GoogleChrome/sw-precache#options-parameter
-  swPrecacheConfig : require('./sw-precache-config.json')
-};
-
-// Add your own custom gulp tasks to the gulp-tasks directory
-// A few sample tasks are provided for you
-// A task should return either a WriteableStream or a Promise
-const clean = require('./gulp-tasks/clean.js');
-const project = require('./gulp-tasks/project.js');
-
-function minify() {
-  return htmlMinifier({
-    collapseWhitespace : true,
-    removeComments : true,
-    removeAttributeQuotes : true,
-    removeRedundantAttributes : true,
-    useShortDoctype : true,
-    removeEmptyAttributes : true,
-    removeScriptTypeAttributes : true,
-    removeStyleLinkTypeAttributes : true,
-    removeOptionalTags : true
-  });
-}
-
-// The source task will split all of your source files into one
-// big ReadableStream. Source files are those in src/** as well as anything
-// added to the sourceGlobs property of polymer.json.
-// Because most HTML Imports contain inline CSS and JS, those inline resources
-// will be split out into temporary files. You can use gulpif to filter files
-// out of the stream and run them through specific tasks. An example is provided
-// which filters all images and runs them through imagemin
-function source() {
-  return project.splitSource()
-      // Add your own build tasks here!
-      .pipe(gulpif(/\.js$/, uglify()))
-      .pipe(gulpif(/\.css$/, cssSlam()))
-      .pipe(gulpif(/\.html$/, cssSlam()))
-      .pipe(gulpif(/\.html$/, minify()))
-      .pipe(project.rejoin());
-}
-
-// The dependencies task will split all of your bower_components files into one
-// big ReadableStream
-// You probably don't need to do anything to your dependencies but it's here in
-// case you need it :)
-function dependencies() {
-  return project.splitDependencies()
-      .pipe(gulpif(/\.js$/, uglify()))
-      .pipe(gulpif(/\.css$/, cssSlam()))
-      .pipe(gulpif(/\.html$/, cssSlam()))
-      .pipe(gulpif(/\.html$/, minify()))
-      .pipe(project.rejoin());
-}
+const project = require('./gulp/project.js');
+const ensureLazyFragments = require('./gulp/ensure-lazy-fragments.js');
 
 const root = path.resolve(process.cwd(), 'images');
 const optimizedImagesRoot = path.resolve(process.cwd(), 'images-optimized');
 const imageOptions = {
-  logos: '250,scale-down'
+  logos: '250,scale-down',
 };
 
 // Optimize images with ImageOptim
@@ -137,18 +64,18 @@ gulp.task('ensure-images-optimized', () =>
   })
 );
 
+gulp.task('ensure-lazy-fragments', ensureLazyFragments);
+
 function linter() {
-  return gulp.src([ 'scripts/**/*.js',
-                    'src/**/*.html' ])
+  return gulp.src('src/**/*.html')
       .pipe(jshint.extract()) // Extract JS from .html files
       .pipe(jshint())
       .pipe(jshint.reporter('jshint-stylish'));
 }
 
 // Clean the build directory, split all source and dependency files into streams
-// and process them, and output bundled and unbundled versions of the project
-// with their own service workers
+// and process them, and output the complete project
 gulp.task('default', gulp.series([
-  clean([ global.config.build.rootDirectory ]), linter,
-  project.merge(source, dependencies), project.serviceWorker
+  linter,
+  project
 ]));
